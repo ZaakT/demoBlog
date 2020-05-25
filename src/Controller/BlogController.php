ec57da77b30f619f42f6e6f264fd0baba804382a
Zakariya TRIFISS <zedaka78@gmail.com>
2020-05-25 17:29:58 +0200

    création gabarit + template + méthode controller

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -7,6 +7,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
+
+    // Un commentaire commençant par @ est une annotation très importante, Symfony explique que lorsque l'on lancera www.monsite.com/blog on fera appel à la méthode index()
+    // Pas besoin de préciser 
+
     /**
      * @Route("/blog", name="blog")
      */
@@ -22,8 +26,12 @@ class BlogController extends AbstractController
      */
     public function home()
     {
-        return $this->render('blog/home.html.twig');
+        return $this->render('blog/home.html.twig', [
+            'title' => 'Bienvenue sur notre blog SYMFONY',
+            'age' => 25
+        ]);
+        
     }
 
-    
+
 }
